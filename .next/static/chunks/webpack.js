95746cde8cc6dc68679753ff25760d3dc901fdda
megpara <meganparadowski@gmail.com>
2024-02-05 19:48:52 -0800

    New files

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "43bf8515952fba890fcf"; }
+/******/ 		__webpack_require__.h = function() { return "6f0800ddafe0da2013a5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -11,15 +11,9 @@ export default function Home() {
 
   return (
       <div className="relative">
-        <img src="*SETTLING_1.jpg" className="w-full" />
-        <img src="*SETTLING_2.jpg" className="w-full" />
-        <img src="*SETTLING_3.jpg" className="w-full" />
-        <img src="*SETTLING_4.jpg" className="w-full" />
-        <img src="*SETTLING_5.jpg" className="w-full" />
-        <img src="*SETTLING_6.jpg" className="w-full" />
-        <img src="*SETTLING_7.jpg" className="w-full" />
-        <img src="*SETTLING_8.jpg" className="w-full" />
-        <img src="*SETTLING_9.jpg" className="w-full" />
+        <img src="settling1_v7.jpg" className="w-full" />
+        <img src="settling4_v7.jpg" className="w-full" />
+        <img src="settling5_v7.jpg" className="w-full" />
         <div className="flex justify-between w-full h-full border-8 border-[#ffcc32]">
             <div className="basis-1/2 border-r-4 border-[#ffcc32]">
               <div
@@ -68,12 +62,15 @@ export default function Home() {
               <script src="https://player.vimeo.com/api/player.js"></script>
             </div>
         </div>
-        <img src="*SETTLING_10.jpg" className="w-full" />
+        <img src="settling6_v7.jpg" className="w-full" />
         <div className="">
-          <a href="https://www.youtube.com/results?search_query=THE+NON-ESSENTIALS+%7C+adult+swim+smalls" target="_blank"><img src="*SETTLING_11.jpg"/></a>
+          <a href="https://www.youtube.com/results?search_query=THE+NON-ESSENTIALS+%7C+adult+swim+smalls" target="_blank"><img src="settling7_v7.jpg"/></a>
         </div>
-        <img src="*SETTLING_12.jpg" className="w-full" />
-        <img src="*SETTLING_13.jpg" className="w-full" />
+        <img src="settling8_v7.jpg" className="w-full" />
+        <img src="settling9_v7.jpg" className="w-full" />
+        <img src="settling10_v7.jpg" className="w-full" />
+        <img src="settling11_v7.jpg" className="w-full" />
+        <img src="settling12_v7.jpg" className="w-full" />
         <button onClick={downloadScript} className="fixed top-0 left-0"><img className="w-[150px] md:w-[200px] pt-4 pl-4 md:pt-8 md:pl-8" src="/SETTLING_DOWNLOAD.png"/></button>
         <div  className="absolute bottom-2 md:bottom-4 w-full flex flex-col items-center">
           <a href="/">
